22b76449ebca1a89ec7a6d9d63659878663a14a0
KirillReal <kiril5471710@gmail.com>
2021-04-15 00:46:11 +0300

    0. MVC в Servlet. [#490586]

--- src/main/java/ru/job4j/dream/servlet/CandidatesServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidatesServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.dream.servlet;
 
+import ru.job4j.dream.store.Store;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +9,5 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CandidatesServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("candidates.jsp").forward(req, resp);
-    }
+
 }
