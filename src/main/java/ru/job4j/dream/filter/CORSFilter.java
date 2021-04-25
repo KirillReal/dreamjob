6eae717cdf2b27d07a73f906b3c539e209a3e421
KirillReal <kiril5471710@gmail.com>
2021-04-26 00:16:31 +0300

    6. CORS [#490552]

--- src/main/java/ru/job4j/dream/filter/CORSFilter.java
+++ src/main/java/ru/job4j/dream/filter/CORSFilter.java
@@ -7,15 +7,18 @@ import java.io.IOException;
 
 public class CORSFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) servletResponse)
+                .addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse)
+                .addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
 
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        if(request.getMethod().equals("OPTIONS")) {
+        if (request.getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_ACCEPTED);
             return;
         }
